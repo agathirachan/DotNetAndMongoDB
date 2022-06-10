fbc1114ac88e990ff16b8b0764c8f53e7b41c4d4
Agat Hirachan <ahirachan@maharam.com>
2022-06-10 16:54:16 -0400

    Some Mongo DB Configurations

---
+++ StudentManagement/Models/StudentStoreDatabaseSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace StudentManagement.Models
+{
+    public class StudentStoreDatabaseSettings : IStudentStoreDatabaseSettings
+    {
+        public string StudentCoursesCollectionName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string ConnectionString { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string DatabaseName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+}
